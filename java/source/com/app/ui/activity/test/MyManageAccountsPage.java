b60fb73767a7fec172c527973a78d9ece8f05446
Valerie Smith <valerielynns@gmail.com>
2013-08-05 16:54:07 -0600

    working out manageAccountsPage kinks

--- java/source/com/app/ui/activity/test/MyManageAccountsPage.java
+++ java/source/com/app/ui/activity/test/MyManageAccountsPage.java
@@ -1,12 +1,14 @@
 package com.app.ui.activity.test;
 
-import com.app.filter.MyUserFilter;
+import com.app.filter.MyAccountUserFilter;
 import com.app.model.MyAccount;
 import com.app.model.MyAccountUser;
 import com.app.model.MyUser;
+import com.app.model.meta.MyMetaAccountUser;
 import com.app.model.meta.MyMetaUser;
 import com.app.utility.MyButtonUrls;
 
+import com.kodemore.adaptor.KmAdaptorIF;
 import com.kodemore.collection.KmList;
 import com.kodemore.filter.KmFilter;
 import com.kodemore.filter.KmFilterFactoryIF;
@@ -22,6 +24,7 @@ import com.kodemore.servlet.control.ScForm;
 import com.kodemore.servlet.control.ScFrame;
 import com.kodemore.servlet.control.ScFrameChild;
 import com.kodemore.servlet.control.ScGrid;
+import com.kodemore.servlet.control.ScGridColumn;
 import com.kodemore.servlet.control.ScGroup;
 import com.kodemore.servlet.control.ScPageRoot;
 import com.kodemore.servlet.field.ScDropdown;
@@ -45,35 +48,35 @@ public class MyManageAccountsPage
     //# variables
     //##################################################
 
-    private ScDropdown     _accountDropdown;
-    private ScDropdown     _editTypeDropdown;
-    private ScDropdown     _addAccountType;
-    private ScDropdown     _editRoleDropdown;
+    private ScDropdown            _accountDropdown;
+    private ScDropdown            _editTypeDropdown;
+    private ScDropdown            _addAccountType;
+    private ScDropdown            _editRoleDropdown;
 
-    private ScFrame        _accountFrame;
+    private ScFrame               _accountFrame;
 
-    private ScTextField    _viewAccountName;
-    private ScTextField    _viewAccountType;
-    private ScTextField    _editAccountName;
-    private ScTextField    _editAccountType;
-    private ScTextField    _addAccountName;
-    private ScTextField    _transferEmail;
-    private ScTextField    _editUserName;
-    private ScTextField    _editUserEmail;
+    private ScTextField           _viewAccountName;
+    private ScTextField           _viewAccountType;
+    private ScTextField           _editAccountName;
+    private ScTextField           _addAccountName;
+    private ScTextField           _transferEmail;
+    private ScTextField           _editUserName;
+    private ScTextField           _editUserEmail;
 
-    private ScFrameChild   _viewAccountChild;
-    private ScFrameChild   _editAccountChild;
-    private ScFrameChild   _addAccountChild;
-    private ScFrameChild   _viewUserChild;
-    private ScFrameChild   _editUserChild;
-    private ScFrameChild   _addUserChild;
+    private ScFrameChild          _viewAccountChild;
+    private ScFrameChild          _editAccountChild;
+    private ScFrameChild          _addAccountChild;
+    private ScFrameChild          _viewUserChild;
+    private ScFrameChild          _editUserChild;
+    private ScFrameChild          _addUserChild;
 
-    private ScFrame        _transferFrame;
-    private ScFrame        _userFrame;
+    private ScFrame               _transferFrame;
+    private ScFrame               _userFrame;
 
-    private ScDialog       _deleteDialog;
+    private ScDialog              _deleteAccountDialog;
 
-    private ScGrid<MyUser> _userGrid;
+    private ScGrid<MyAccountUser> _userGrid;
+    private ScFrameChild          _transferChild;
 
     //##################################################
     //# install
@@ -86,7 +89,7 @@ public class MyManageAccountsPage
         root = newPageRoot();
         root.css().padSpaced();
 
-        installDialog(root);
+        installDeleteAccountDialog(root);
         installAccountsDropdown(root);
 
         ScArray row;
@@ -104,20 +107,20 @@ public class MyManageAccountsPage
         return root;
     }
 
-    private void installDialog(ScBox root)
+    private void installDeleteAccountDialog(ScBox root)
     {
-        _deleteDialog = root.addDialog();
-        _deleteDialog.getHeaderBox().hide();
-        _deleteDialog.getFooterBox().hide();
+        _deleteAccountDialog = root.addDialog();
+        _deleteAccountDialog.getHeaderBox().hide();
+        _deleteAccountDialog.getFooterBox().hide();
 
-        ScBox body = _deleteDialog.getBodyBox();
+        ScBox body = _deleteAccountDialog.getBodyBox();
 
         ScGroup group;
-        group = body.addGroup("Are you sure you want to delete this account user?");
+        group = body.addGroup("Are you sure you want to delete this account?");
 
         ScArray row;
         row = group.addRow();
-        row.addPad().addButton("Yes", newDeleteAction());
+        row.addPad().addButton("Yes", newDeleteAccountAction());
         row.addPad().addButton("No", newCloseAction());
     }
 
@@ -128,8 +131,11 @@ public class MyManageAccountsPage
         ScGroup group;
         group = root.addGroup("Manage Accounts");
 
+        ScForm form;
+        form = group.addForm();
+
         ScBox body;
-        body = group.addPadSpaced();
+        body = form.addPadSpaced();
         body.add(_accountDropdown);
         body.addButton("Test", showView());
         body.addButton("Add", newShowAddAccountBoxAction());
@@ -207,6 +213,8 @@ public class MyManageAccountsPage
         ScDiv footer;
         footer = group.addButtonBox();
         footer.addButton("Send Request", newSendTransferRequestAction());
+
+        _transferChild = frame;
     }
 
     private void installEditAccountFrame()
@@ -232,14 +240,14 @@ public class MyManageAccountsPage
         _editAccountName = new ScTextField();
         _editAccountName.setLabel("Account name is ");
 
-        _editAccountType = new ScTextField();
-        _editAccountType.setLabel("Account type is ");
+        _editTypeDropdown = MyAccount.Tools.newTypeDropdown();
+        _editTypeDropdown.setLabel("Account type is ");
 
         ScFieldTable fields;
         fields = body.addFields();
         fields.add(_editAccountName);
         fields.addSpace();
-        fields.add(_editAccountType);
+        fields.add(_editTypeDropdown);
 
         group.addDivider();
 
@@ -294,7 +302,7 @@ public class MyManageAccountsPage
 
     private void installUserGrid(ScArray root)
     {
-        MyMetaUser x = MyUser.Meta;
+        MyMetaAccountUser x = MyAccountUser.Meta;
 
         ScActionIF addAction;
         addAction = newAddUserAction();
@@ -311,32 +319,62 @@ public class MyManageAccountsPage
         button = right.addButton("Add", addAction);
         button.setImage(MyButtonUrls.add());
 
-        ScGrid<MyUser> grid;
+        ScGridColumn<MyAccountUser> userEmail;
+        userEmail = new ScGridColumn<MyAccountUser>();
+        userEmail.setDisplayAdaptor(newUserEmailDisplayAdaptor());
+        userEmail.setHeader("User Email");
+
+        ScGrid<MyAccountUser> grid;
         grid = group.addGrid();
         grid.setFilterFactory(newFetcher());
-        grid.addLinkColumn(x.Name, newViewUserAction(), x.Uid);
-        grid.addColumn(x.Email);
+        grid.addLinkColumn(x.UserName, newViewUserAction(), x.Uid);
+        grid.addColumn(userEmail);
 
         _userGrid = grid;
     }
 
-    private KmFilterFactoryIF<MyUser> newFetcher()
+    private KmAdaptorIF<MyAccountUser,String> newUserEmailDisplayAdaptor()
     {
-        return new KmFilterFactoryIF<MyUser>()
+        return new KmAdaptorIF<MyAccountUser,String>()
         {
             @Override
-            public KmFilter<MyUser> createFilter()
+            public String getValue(MyAccountUser model)
+            {
+                if ( model.getUser() != null )
+                    return model.getUser().getEmail();
+                return "";
+            }
+
+            @Override
+            public void setValue(MyAccountUser model, String value)
+            {
+                //none
+            }
+        };
+    }
+
+    private KmFilterFactoryIF<MyAccountUser> newFetcher()
+    {
+        return new KmFilterFactoryIF<MyAccountUser>()
+        {
+            @Override
+            public KmFilter<MyAccountUser> createFilter()
             {
                 return newUserFilter();
             }
         };
     }
 
-    private KmFilter<MyUser> newUserFilter()
+    private KmFilter<MyAccountUser> newUserFilter()
     {
-        MyUserFilter f;
-        f = new MyUserFilter();
-        f.sortOnName();
+        MyAccountUserFilter f;
+        f = new MyAccountUserFilter();
+        f.sortAscending();
+
+        // fixme_valerie: need an onChange method for dropdown
+        String accountName = _accountDropdown.getStringValue();
+        MyAccount a = getAccess().getAccountDao().findWithName(accountName);
+        f.setAccountUid(a.getUid());
 
         return f;
     }
@@ -353,6 +391,7 @@ public class MyManageAccountsPage
     private void installViewUserFrame()
     {
         MyMetaUser x = MyUser.Meta;
+        MyMetaAccountUser y = MyAccountUser.Meta;
 
         ScFrameChild frame;
         frame = _userFrame.createChild();
@@ -364,10 +403,6 @@ public class MyManageAccountsPage
         header = group.getHeader().addFloatRight();
         header.css().pad5();
 
-        ScActionButton button;
-        button = header.addButton("Edit", newShowEditUserBoxAction());
-        button.setImage(MyButtonUrls.edit());
-
         ScBox body;
         body = group.addBox();
         body.css().pad();
@@ -376,7 +411,19 @@ public class MyManageAccountsPage
         fields = body.addFields();
         fields.addText(x.Name);
         fields.addText(x.Email);
-        fields.addText(x.RoleName);
+        fields.addText(y.RoleName);
+
+        group.addDivider();
+
+        ScDiv footer;
+        footer = group.addButtonBoxRight();
+
+        ScActionButton button;
+        button = footer.addButton("Edit", newShowEditUserBoxAction());
+        button.setImage(MyButtonUrls.edit());
+
+        // fixme_valerie: add this dialog
+        //        footer.addButton("Remove from Account", newShowDeleteAccountUserDialogAction());
 
         _viewUserChild = frame;
     }
@@ -403,9 +450,11 @@ public class MyManageAccountsPage
 
         _editUserName = new ScTextField();
         _editUserName.setLabel("User name is ");
+        _editUserName.setReadOnly();
 
         _editUserEmail = new ScTextField();
         _editUserEmail.setLabel("User's email is ");
+        _editUserEmail.setReadOnly();
 
         _editRoleDropdown = MyAccountUser.Tools.newRoleDropdown();
         _editRoleDropdown.setLabel("User role is ");
@@ -414,7 +463,6 @@ public class MyManageAccountsPage
         fields = body.addFields();
         fields.add(_editUserName);
         fields.add(_editUserEmail);
-        fields.add(_editAccountName);
         fields.add(_editRoleDropdown);
 
         group.addDivider();
@@ -430,20 +478,21 @@ public class MyManageAccountsPage
     private void installAddUserFrame()
     {
         MyMetaUser x = MyUser.Meta;
+        MyMetaAccountUser y = MyAccountUser.Meta;
 
-        //        ScActionIF saveAction = newAddSaveAction();
-        //        ScActionIF cancelAction = newAddCancelAction();
+        ScActionIF sendAction = newAddUserSendEmailAction();
+        ScActionIF cancelAction = newAddUserCancelAction();
 
         ScFrameChild frame;
         frame = _userFrame.createChild();
 
         ScForm form;
         form = frame.addForm();
-        //        form.setDefaultAction(saveAction);
-        //        form.onEscape().run(cancelAction);
+        form.setDefaultAction(sendAction);
+        form.onEscape().run(cancelAction);
 
         ScGroup group;
-        group = form.addGroup("Add");
+        group = form.addGroup("Invite User to " + getPageSession().getAccount());
 
         ScBox body;
         body = group.addBox();
@@ -452,14 +501,14 @@ public class MyManageAccountsPage
         ScFieldTable fields;
         fields = body.addFields();
         fields.addField(x.Email);
-        fields.addField(x.Name);
+        fields.addField(y.RoleName);
 
         group.addDivider();
 
         ScDiv footer;
         footer = group.addButtonBoxRight();
-        //        footer.addCancelButton(cancelAction);
-        footer.addSubmitButton("Save");
+        footer.addCancelButton(cancelAction);
+        footer.addSubmitButton("Send Request");
 
         _addUserChild = frame;
     }
@@ -480,14 +529,14 @@ public class MyManageAccountsPage
         };
     }
 
-    private ScActionIF newDeleteAction()
+    private ScActionIF newDeleteAccountAction()
     {
         return new ScAction(this)
         {
             @Override
             public void handle()
             {
-                handleDelete();
+                handleDeleteAccount();
             }
         };
     }
@@ -660,6 +709,30 @@ public class MyManageAccountsPage
         };
     }
 
+    private ScActionIF newAddUserSendEmailAction()
+    {
+        return new ScAction(this)
+        {
+            @Override
+            public void handle()
+            {
+                handleAddUserSendEmail();
+            }
+        };
+    }
+
+    private ScActionIF newAddUserCancelAction()
+    {
+        return new ScAction(this)
+        {
+            @Override
+            public void handle()
+            {
+                handleAddUserCancel();
+            }
+        };
+    }
+
     private ScActionIF newShowEditUserBoxAction()
     {
         return new ScAction(this)
@@ -688,9 +761,8 @@ public class MyManageAccountsPage
         for ( MyAccount account : accounts )
             accountNames.add(account.getName());
 
-        // fixme_valerie: 
         _accountDropdown.setOptions(accountNames);
-        //        _dropdown.ajaxUpdateValues();
+        _accountDropdown.ajaxUpdateValues();
 
         super.start();
     }
@@ -705,25 +777,25 @@ public class MyManageAccountsPage
         e = getAccess().findAccountUserUid(getStringArgument());
         getPageSession().setAccountUser(e);
 
-        _deleteDialog.ajaxOpen();
+        _deleteAccountDialog.ajaxOpen();
     }
 
-    private void handleDelete()
+    private void handleDeleteAccount()
     {
-        MyAccountUser e;
-        e = getPageSession().getAccountUser();
+        MyAccount e;
+        e = getPageSession().getAccount();
         e.deleteDao();
 
-        _deleteDialog.ajaxClose();
+        _deleteAccountDialog.ajaxClose();
 
-        ajax().toast("Deleted account user %s", e.getUid());
+        ajax().toast("Deleted account %s", e.getName());
 
         _accountDropdown.ajaxUpdateValues();
     }
 
     private void handleClose()
     {
-        _deleteDialog.ajaxClose();
+        _deleteAccountDialog.ajaxClose();
     }
 
     private void handleShowView()
@@ -733,8 +805,6 @@ public class MyManageAccountsPage
 
         MyAccount account;
         account = getAccess().getAccountDao().findWithName(accountName);
-        //remove_valerie: println
-        System.out.println("    accountName: " + accountName);
         getPageSession().setAccount(account);
 
         _viewAccountName.setValue(account.getName());
@@ -767,7 +837,8 @@ public class MyManageAccountsPage
 
     private void handleShowTransferBox()
     {
-        _transferFrame.ajaxPrint();
+        _transferChild.ajaxPrint();
+        _transferChild.ajax().focus();
     }
 
     private void handleSendTransferRequest()
@@ -868,6 +939,23 @@ public class MyManageAccountsPage
         _viewUserChild.ajaxPrint();
     }
 
+    private void handleAddUserSendEmail()
+    {
+        /**
+         * todo_valerie hook up email request
+         */
+    }
+
+    private void handleAddUserCancel()
+    {
+        _userFrame.ajaxClear();
+    }
+
+    private void handleEditUserCancel()
+    {
+        _userFrame.ajaxClear();
+    }
+
     private void handleShowEditUserBox()
     {
         MyAccountUser e;
@@ -892,26 +980,8 @@ public class MyManageAccountsPage
     {
         _editUserChild.validate();
 
-        if ( !_editUserName.hasValue() )
-        {
-            ajax().toast("Please enter a user name");
-            return;
-        }
-
-        if ( !_editUserEmail.hasValue() )
-        {
-            ajax().toast("Please enter the user's email");
-            return;
-        }
-
         MyUser user;
         user = getPageSession().getUser();
-
-        if ( user == null )
-            user = new MyUser();
-
-        user.setName(_editUserName.getValue());
-        user.setEmail(_editUserEmail.getValue());
 
         MyAccount account;
         account = getPageSession().getAccount();
@@ -926,9 +996,6 @@ public class MyManageAccountsPage
         accountUser.setAccount(account);
         accountUser.setUser(user);
 
-        if ( _editTypeDropdown.hasValue() )
-            account.setTypeCode(_editTypeDropdown.getStringValue());
-
         if ( _editRoleDropdown.hasValue() )
             accountUser.setRoleCode(_editRoleDropdown.getStringValue());
 
@@ -939,8 +1006,4 @@ public class MyManageAccountsPage
         _userGrid.ajaxReload();
     }
 
-    private void handleEditUserCancel()
-    {
-        _userFrame.ajaxClear();
-    }
 }
--- java/source/com/app/ui/servlet/base/MyActivityRegistryBase.java
+++ java/source/com/app/ui/servlet/base/MyActivityRegistryBase.java
@@ -8,65 +8,14 @@
 
 package com.app.ui.servlet.base;
 
-import com.app.ui.activity.admin.MyAdminPage;
-import com.app.ui.activity.admin.MyEmailEditPage;
-import com.app.ui.activity.admin.MyEmailListPage;
-import com.app.ui.activity.admin.MyEmailViewPage;
-import com.app.ui.activity.admin.MyUsersPage;
-import com.app.ui.activity.general.MyHomePage;
-import com.app.ui.activity.general.MySignOutPage;
-import com.app.ui.activity.general.MyTimeoutPage;
-import com.app.ui.activity.login.MyHandleInvitationActivity;
-import com.app.ui.activity.login.MyHandlePasswordResetActivity;
-import com.app.ui.activity.login.MySignInActivity;
-import com.app.ui.activity.test.MyAccountTestPage;
-import com.app.ui.activity.test.MyAccountUserTestPage;
-import com.app.ui.activity.test.MyAnimationTestPage;
-import com.app.ui.activity.test.MyAutoCompleteTestPage;
-import com.app.ui.activity.test.MyBarcodeTestPage;
-import com.app.ui.activity.test.MyBlankTestPage;
-import com.app.ui.activity.test.MyBlockTestPage;
-import com.app.ui.activity.test.MyColorFieldTestPage;
-import com.app.ui.activity.test.MyDateFieldTestPage;
-import com.app.ui.activity.test.MyDomainDropdownSetValueTestPage;
-import com.app.ui.activity.test.MyDownloadTestPage;
-import com.app.ui.activity.test.MyDropzoneTestPage;
-import com.app.ui.activity.test.MyFieldTestPage;
-import com.app.ui.activity.test.MyFilterTableRowTestPage;
-import com.app.ui.activity.test.MyFormTestPage;
-import com.app.ui.activity.test.MyGmailTestPage;
-import com.app.ui.activity.test.MyGoogleChartTestPage;
-import com.app.ui.activity.test.MyGridTestPage;
-import com.app.ui.activity.test.MyGroupIconHeaderTestPage;
-import com.app.ui.activity.test.MyGroupTestPage;
-import com.app.ui.activity.test.MyHideErrorsTestPage;
-import com.app.ui.activity.test.MyLocalValueTestPage;
-import com.app.ui.activity.test.MyManageAccountsPage;
-import com.app.ui.activity.test.MyMemoryLeakTestPage;
-import com.app.ui.activity.test.MyNotebookTestPage;
-import com.app.ui.activity.test.MyOpenWindowTestPage;
-import com.app.ui.activity.test.MyPlaceholderTestPage;
-import com.app.ui.activity.test.MyQuickTestPage;
-import com.app.ui.activity.test.MyScriptTestPage;
-import com.app.ui.activity.test.MyShowDialogTestPage;
-import com.app.ui.activity.test.MySlowTestPage;
-import com.app.ui.activity.test.MyStaticIncludeTestPage;
-import com.app.ui.activity.test.MyTabManagerTestPage;
-import com.app.ui.activity.test.MyTestPage;
-import com.app.ui.activity.test.MyToastTestPage;
-import com.app.ui.activity.test.MyWelcomePage;
-import com.app.ui.activity.tools.MyBeanShellPage;
-import com.app.ui.activity.tools.MyPerformanceLogPage;
-import com.app.ui.activity.tools.MyPropertiesPage;
-import com.app.ui.activity.tools.MySharedFileBrowserPage;
-import com.app.ui.activity.tools.MySqlPage;
-import com.app.ui.activity.tools.MySystemLogListPage;
-import com.app.ui.activity.tools.MyToolsPage;
-import com.app.ui.activity.tools.MyUtilityPage;
-import com.app.ui.activity.user.MyUserPasswordPage;
-import com.app.ui.activity.user.MyUserSettingsPage;
+import com.kodemore.servlet.*;
 
-import com.kodemore.servlet.ScActivityRegistry;
+import com.app.ui.activity.admin.*;
+import com.app.ui.activity.general.*;
+import com.app.ui.activity.login.*;
+import com.app.ui.activity.test.*;
+import com.app.ui.activity.tools.*;
+import com.app.ui.activity.user.*;
 
 public abstract class MyActivityRegistryBase
     extends ScActivityRegistry
@@ -75,7 +24,7 @@ public abstract class MyActivityRegistryBase
     protected void registerActivities()
     {
         super.registerActivities();
-
+        
         add(MyAccountTestPage.instance);
         add(MyAccountUserTestPage.instance);
         add(MyAdminPage.instance);
