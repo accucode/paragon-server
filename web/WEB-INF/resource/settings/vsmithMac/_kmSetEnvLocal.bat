cd816bf5f891e5a450cc06abded583faaeadd61f
vlsmith <valerielynns@gmail.com>
2013-08-25 15:27:01 -0600

    Mac setup

---
+++ web/WEB-INF/resource/settings/vsmithMac/_kmSetEnvLocal.bat
@@ -0,0 +1,3 @@
+set java_home=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk
+set ant_home=/usr/share/java/ant-1.8.4/bin/ant
+set tomcat_home=/Applications/BitNamiStackTomcat.app/Contents/Resources/app/apache-tomcat
