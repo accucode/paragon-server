f8ca2aacf2a53b06eab92bd3f2f9fd8d59270397
Valerie Smith <valerielynns@gmail.com>
2013-10-07 11:27:45 -0600

    testing

--- java/source/sandbox/wlove/JkValerie.java
+++ java/source/sandbox/wlove/JkValerie.java
@@ -3,6 +3,7 @@ package sandbox.wlove;
 import com.kodemore.http.KmHttpGet;
 import com.kodemore.http.KmHttpRequest;
 
+// review_valerie: simple page to receive public information
 public class JkValerie
 {
     //##################################################
@@ -24,7 +25,10 @@ public class JkValerie
         e = new KmHttpGet();
         e.setHost("graph.facebook.com");
         e.setPort(80);
-        e.setPath("cocacola");
+        e.setPath("oauth/access_token");
+        // fixme_valerie: insert client id from dashboard
+        e.setParameter("client_id", "");
+        e.setParameter("redirect_uri", "http://www.accucode.com/");
         e.setContentType("text/html");
         e.submit();
         System.out.println("------------------------------------------------------------");
