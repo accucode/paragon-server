49191cfb437f4138e302dd4e255801d742d88448
Wyatt Love <wlove@accucode.com>
2013-12-26 10:21:32 -0700

    Removed obsolete pages.

--- java/source/com/app/ui/page/admin/MyAdminPage.java
+++ java/source/com/app/ui/page/admin/MyAdminPage.java
@@ -6,7 +6,6 @@ import com.kodemore.servlet.control.ScGroupArray;
 import com.kodemore.servlet.control.ScPageRoot;
 
 import com.app.ui.page.admin.accounts.MyAccountSettingsPage;
-import com.app.ui.page.admin.accounts.MyAccountsPage;
 import com.app.ui.page.admin.accounts.MySelectAccountPage;
 import com.app.ui.page.admin.userProfile.MyUserProfilePage;
 
@@ -48,7 +47,6 @@ public class MyAdminPage
         group.addDivider();
 
         links = group.addLinkBox();
-        links.addLink(MyAccountsPage.instance);
         links.addLink(MyUserProfilePage.instance);
     }
 }
--- java/source/com/app/ui/servlet/base/MyPageRegistryBase.java
+++ java/source/com/app/ui/servlet/base/MyPageRegistryBase.java
@@ -8,16 +8,80 @@
 
 package com.app.ui.servlet.base;
 
-import com.kodemore.servlet.*;
+import com.kodemore.servlet.ScPageRegistry;
 
-import com.app.ui.page.admin.*;
-import com.app.ui.page.admin.accounts.*;
-import com.app.ui.page.admin.userProfile.*;
-import com.app.ui.page.general.*;
-import com.app.ui.page.login.*;
-import com.app.ui.page.test.*;
-import com.app.ui.page.tools.*;
-import com.app.ui.page.user.*;
+import com.app.ui.page.admin.MyAdminPage;
+import com.app.ui.page.admin.MyDevEmailEditPage;
+import com.app.ui.page.admin.MyDevEmailListPage;
+import com.app.ui.page.admin.MyDevEmailViewPage;
+import com.app.ui.page.admin.MyDevUsersPage;
+import com.app.ui.page.admin.accounts.MyAccountSettingsPage;
+import com.app.ui.page.admin.accounts.MySelectAccountPage;
+import com.app.ui.page.admin.userProfile.MyUserProfilePage;
+import com.app.ui.page.general.MyHomePage;
+import com.app.ui.page.general.MySignOutPage;
+import com.app.ui.page.general.MyTimeoutPage;
+import com.app.ui.page.login.MyPasswordResetPage;
+import com.app.ui.page.login.MySignInPage;
+import com.app.ui.page.login.MyUserActivationPage;
+import com.app.ui.page.test.MyAccordionTestPage;
+import com.app.ui.page.test.MyAnimationTestPage;
+import com.app.ui.page.test.MyAutoCompleteTestPage;
+import com.app.ui.page.test.MyBarcodeTestPage;
+import com.app.ui.page.test.MyBlankTestPage;
+import com.app.ui.page.test.MyBlockTestPage;
+import com.app.ui.page.test.MyBorderTestPage;
+import com.app.ui.page.test.MyCardFlipTestPage;
+import com.app.ui.page.test.MyChartTestPage;
+import com.app.ui.page.test.MyColorFieldTestPage;
+import com.app.ui.page.test.MyDateFieldTestPage;
+import com.app.ui.page.test.MyDomainDropdownSetValueTestPage;
+import com.app.ui.page.test.MyDownloadTestPage;
+import com.app.ui.page.test.MyDragScrollToTestPage;
+import com.app.ui.page.test.MyDropzoneTestPage;
+import com.app.ui.page.test.MyEqualizeTestPage;
+import com.app.ui.page.test.MyFacebookTestPage;
+import com.app.ui.page.test.MyFieldTestPage;
+import com.app.ui.page.test.MyFilterTableRowTestPage;
+import com.app.ui.page.test.MyFormTestPage;
+import com.app.ui.page.test.MyGmailTestPage;
+import com.app.ui.page.test.MyGoogleChartTestPage;
+import com.app.ui.page.test.MyGradientTestPage;
+import com.app.ui.page.test.MyGridTestPage;
+import com.app.ui.page.test.MyGroupIconHeaderTestPage;
+import com.app.ui.page.test.MyGroupTestPage;
+import com.app.ui.page.test.MyHandleSortTestPage;
+import com.app.ui.page.test.MyHideErrorsTestPage;
+import com.app.ui.page.test.MyHoverTestPage;
+import com.app.ui.page.test.MyImageButtonTestPage;
+import com.app.ui.page.test.MyLocalValueTestPage;
+import com.app.ui.page.test.MyMemoryLeakTestPage;
+import com.app.ui.page.test.MyNotebookTestPage;
+import com.app.ui.page.test.MyOpenWindowTestPage;
+import com.app.ui.page.test.MyPaddingTestPage;
+import com.app.ui.page.test.MyPageSessionTest1Page;
+import com.app.ui.page.test.MyPageSessionTest2Page;
+import com.app.ui.page.test.MyPlaceholderTestPage;
+import com.app.ui.page.test.MyPopupMenuTestPage;
+import com.app.ui.page.test.MyRadioButtonTestPage;
+import com.app.ui.page.test.MyScriptTestPage;
+import com.app.ui.page.test.MyShowDialogTestPage;
+import com.app.ui.page.test.MySlowTestPage;
+import com.app.ui.page.test.MySmtpTestPage;
+import com.app.ui.page.test.MyStaticIncludeTestPage;
+import com.app.ui.page.test.MyTabbedTestPage;
+import com.app.ui.page.test.MyTestPage;
+import com.app.ui.page.test.MyToastTestPage;
+import com.app.ui.page.test.MyWyattTestPage;
+import com.app.ui.page.tools.MyDevApplicationPropertiesPage;
+import com.app.ui.page.tools.MyDevBeanShellPage;
+import com.app.ui.page.tools.MyDevPerformanceLogPage;
+import com.app.ui.page.tools.MyDevSharedFileBrowserPage;
+import com.app.ui.page.tools.MyDevSqlPage;
+import com.app.ui.page.tools.MyDevSystemLogListPage;
+import com.app.ui.page.tools.MyDevSystemPropertiesPage;
+import com.app.ui.page.tools.MyDevToolsPage;
+import com.app.ui.page.tools.MyDevUtilityPage;
 
 public abstract class MyPageRegistryBase
     extends ScPageRegistry
@@ -26,10 +90,9 @@ public abstract class MyPageRegistryBase
     protected void registerPages()
     {
         super.registerPages();
-        
+
         add(MyAccordionTestPage.instance);
         add(MyAccountSettingsPage.instance);
-        add(MyAccountsPage.instance);
         add(MyAdminPage.instance);
         add(MyAnimationTestPage.instance);
         add(MyAutoCompleteTestPage.instance);
@@ -98,9 +161,7 @@ public abstract class MyPageRegistryBase
         add(MyTimeoutPage.instance);
         add(MyToastTestPage.instance);
         add(MyUserActivationPage.instance);
-        add(MyUserPasswordPage.instance);
         add(MyUserProfilePage.instance);
-        add(MyUserSettingsPage.instance);
         add(MyWyattTestPage.instance);
     }
 }
