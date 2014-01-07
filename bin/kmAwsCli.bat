fa1d60305a127fb64e6c7a3851838c497e984abd
rwaxler <rwaxler@accucode.com>
2014-01-07 10:25:51 -0700

    added default region to command

--- bin/kmAwsCli.bat
+++ bin/kmAwsCli.bat
@@ -4,7 +4,7 @@
 pushd ..\..
 
 echo %awscli_home%\aws %1 %2 %3 %4 %5 %6 %7 %8 %9
-"%awscli_home%\aws" %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%awscli_home%\aws" --region us-east-1 %1 %2 %3 %4 %5 %6 %7 %8 %9
 set cmdResult=%errorlevel%
 
 popd
